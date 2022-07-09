cd44aad13d29174d11924625b7fa57a006a2ee57
shunmame <32508729+shunmame@users.noreply.github.com>
2022-07-09 14:02:00 +0900

    スライダーの範囲設定

--- js/maze_search.js
+++ js/maze_search.js
@@ -1,14 +1,50 @@
+var column_names_jp = {
+    "1次的な特徴量": ["直線のマス数", "T字路のマス数", "十字路のマス数", "曲がり角のマス数", "行き止まりのマス数"],
+    "2次的な特徴量": [/*"直線の長さ",*/ "直線の長さの標準偏差",
+                     "正道の経路の長さ", "正道の曲がり角のマス数",
+                     "誤道の周辺長", "誤道の隣接数",
+                     "L領域の誤道数", "L領域の総経路長", "L領域の経路の標準偏差",
+                     "R領域の誤道数", "R領域の総経路長", "R領域の経路の標準偏差",
+                    ],
+    "3次的な特徴量": ["ゴールまでの深さ", "最大深さ", "深さの平均", "深さの標準偏差"],
+}
+var column_names_en = {
+    "1次的な特徴量": ["straight_num", "T_num", "cross_num", "turn_pos_num", "dead_end_num"],
+    "2次的な特徴量": [/*"straight_len_log_slope",*/ "straight_len_std",
+                     "correct_path_len", "turn_num",
+                     "boader_l", "neighbor_sum",
+                     "L_num", "L_size", "L_size_std",
+                     "R_num", "R_size", "R_size_std",
+                    ],
+    "3次的な特徴量": ["gorl_depth", "depth_max", "depth_mean", "depth_std"],
+}
+
+var feature_max_min_dict = {
+    'turn_num': {'max': 224, 'min': 1, 'digit': 0},
+    'L_num': {'max': 40, 'min': 0, 'digit': 0},
+    'L_size': {'max': 1072, 'min': 0, 'digit': 0},
+    'L_size_std': {'max': 384.035, 'min': 0.0, 'digit': 3},
+    'R_num': {'max': 41, 'min': 0, 'digit': 0},
+    'R_size': {'max': 1140, 'min': 0, 'digit': 0},
+    'R_size_std': {'max': 294.118, 'min': 0.0, 'digit': 3},
+    'boader_l': {'max': 1311, 'min': 0, 'digit': 0},
+    'neighbor_sum': {'max': 133, 'min': 0, 'digit': 0},
+    'correct_path_len': {'max': 705, 'min': 9, 'digit': 0},
+    'turn_pos_num': {'max': 337, 'min': 0, 'digit': 0},
+    'dead_end_num': {'max': 235, 'min': 2, 'digit': 0},
+    'T_num': {'max': 166, 'min': 0, 'digit': 0},
+    'cross_num': {'max': 51, 'min': 0, 'digit': 0},
+    'straight_len_std': {'max': 5.195, 'min': 0.0, 'digit': 3},
+    'straight_num': {'max': 553, 'min': 4, 'digit': 0},
+    'gorl_depth': {'max': 704, 'min': 8, 'digit': 0},
+    'depth_std': {'max': 242.339, 'min': 0.0, 'digit': 3},
+    'depth_max': {'max': 798, 'min': 8, 'digit': 0},
+    'depth_mean': {'max': 463.86, 'min': 5.2, 'digit': 3},
+    'straight_len_log_slope': {'max': 0.331, 'min': -0.628, 'digit': 3}
+}
+
+
 function createSearchBox(){
-    var column_names_jp = {
-        "1次的な特徴量": ["直線のマス数", "T字路のマス数", "十字路のマス数", "曲がり角のマス数", "行き止まりのマス数"],
-        "2次的な特徴量": [/*"直線の長さ"*/, "直線の長さの標準偏差",
-                         "正道の経路の長さ", "正道の曲がり角のマス数",
-                         "誤道の周辺長", "誤道の隣接数",
-                         "L領域の誤道数", "L領域の総経路長", "L領域の経路の標準偏差",
-                         "R領域の誤道数", "R領域の総経路長", "R領域の経路の標準偏差",
-                        ],
-        "3次的な特徴量": ["ゴールまでの深さ", "最大深さ", "深さの平均", "深さの標準偏差"],
-    }
     search_box_div = document.getElementById("search_box")
 
     for (var key in column_names_jp){
@@ -39,23 +75,18 @@ function createSearchBox(){
             slider_div.id = "slider"
 
             noUiSlider.create(slider_div, {
-                start: [20, 80],
+                start: [
+                    feature_max_min_dict[column_names_en[key][i]]["min"], 
+                    feature_max_min_dict[column_names_en[key][i]]["max"]
+                ],
                 connect: true,
                 range: {
-                    'min': 0,
-                    'max': 100
+                    'min': feature_max_min_dict[column_names_en[key][i]]["min"],
+                    'max': feature_max_min_dict[column_names_en[key][i]]["max"]
                 },
-                format: {
-                    to: function ( value ) {
-                      return parseInt(value);
-                    },
-                    from: function ( value ) {
-                      return value;
-                    }
-                  },
                 tooltips: [
-                    wNumb({decimals: 0}),
-                    true
+                    wNumb({decimals: feature_max_min_dict[column_names_en[key][i]]["digit"]}),
+                    wNumb({decimals: feature_max_min_dict[column_names_en[key][i]]["digit"]}),
                 ],
             });
 
