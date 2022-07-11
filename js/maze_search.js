166a8bba037a6359cdf1bbf1ec95f6efb214c3ae
Kiharalabcode <101311077+Kiharalabcode@users.noreply.github.com>
2022-07-11 19:17:09 +0900

    Update maze_search.js

--- js/maze_search.js
+++ js/maze_search.js
@@ -158,7 +158,7 @@ createSizeSlider()
 createSearchBox()
 
 function searchButtonClick() {
-    var baseURL = "http://127.0.0.1:5500/maze_result.html"
+    var baseURL = "https://kiharalabcode.github.io/maze-site/maze_result.html"
     var params_str = "?algo="
 
     var algo_names_list = ["AldousBroder", "Division", "GrowingTree", "Kruskal", "Prims", "Sidewinder"]
